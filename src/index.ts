5b27e4c5c39c908a67293841124c0b5727990983
Jeppe Reinhold <jeppe@reinhold.is>
2022-07-20 21:11:24 +0200

    format, fix tests

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import {getInput, debug, setOutput, setFailed} from '@actions/core'
-import {wait} from './wait'
+import { getInput, debug, setOutput, setFailed } from '@actions/core';
+import { wait } from './wait';
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = getInput('milliseconds')
-    debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+	try {
+		const ms: string = getInput('milliseconds');
+		debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    debug(new Date().toTimeString())
+		debug(new Date().toTimeString());
+		await wait(parseInt(ms, 10));
+		debug(new Date().toTimeString());
 
-    setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) setFailed(error.message)
-  }
+		setOutput('time', new Date().toTimeString());
+	} catch (error) {
+		if (error instanceof Error) setFailed(error.message);
+	}
 }
 
-run()
+run();
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,8 @@
-import {defineConfig} from 'tsup'
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['./src/index.ts'],
-  sourcemap: true,
-  clean: true,
-  target: 'node16'
-})
+	entry: ['./src/index.ts'],
+	sourcemap: true,
+	clean: true,
+	target: 'node16',
+});
