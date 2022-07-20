87fc7754336ef45af692fae084362351622105a8
Jeppe Reinhold <jeppe@reinhold.is>
2022-07-20 23:18:45 +0200

    make action runnable

--- src/run.ts
+++ src/run.ts
@@ -1,13 +1,8 @@
-import { getInput, debug, setOutput, setFailed } from '@actions/core';
-import { wait } from './wait';
+import { debug, setOutput, setFailed } from '@actions/core';
 
 export const run = async (): Promise<void> => {
 	try {
-		const ms: string = getInput('milliseconds');
-		debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
 		debug(new Date().toTimeString());
-		await wait(parseInt(ms, 10));
 		debug(new Date().toTimeString());
 
 		setOutput('time', new Date().toTimeString());
